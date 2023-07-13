07afb5ad7db94c19815250517ec739f5becb2de4
JoJo <joehearthstone@gmail.com>
2023-07-13 13:14:31 -0400

    please work

--- app/models/user_entry.py
+++ app/models/user_entry.py
@@ -4,6 +4,6 @@ user_entry = db.Table(
     add_prefix_for_prod('user_entry'),
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
     db.Column('entry_id', db.Integer, db.ForeignKey(add_prefix_for_prod('entries.id')), primary_key=True),
-    schema=SCHEMA if environment == "production" else None
+    schema=add_prefix_for_prod(SCHEMA) if environment == "production" else None
 
 )
--- app/models/user_review.py
+++ app/models/user_review.py
@@ -4,5 +4,5 @@ user_review = db.Table(
     add_prefix_for_prod('user_review'),
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
     db.Column('review_id', db.Integer, db.ForeignKey(add_prefix_for_prod('reviews.id')), primary_key=True),
-    schema=SCHEMA if environment == "production" else None
+    schema=add_prefix_for_prod(SCHEMA) if environment == "production" else None
 )
