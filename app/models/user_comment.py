0a9449cdea5c228f76be51a9f21693a073906dc0
JoJo <joehearthstone@gmail.com>
2023-07-13 13:09:49 -0400

    ok maybe

--- app/models/user_comment.py
+++ app/models/user_comment.py
@@ -4,7 +4,6 @@ from .db import db, SCHEMA, environment,add_prefix_for_prod
 user_comment = db.Table(
     add_prefix_for_prod('user_comment'),
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
-    db.Column('comment_id', db.Integer, db.ForeignKey(add_prefix_for_prod('comments.id')), primary_key=True)
+    db.Column('comment_id', db.Integer, db.ForeignKey(add_prefix_for_prod('comments.id')), primary_key=True),
+    schema=SCHEMA if environment == "production" else None
 )
-
-
--- app/models/user_review.py
+++ app/models/user_review.py
@@ -3,6 +3,6 @@ from .db import db, SCHEMA, environment, add_prefix_for_prod
 user_review = db.Table(
     add_prefix_for_prod('user_review'),
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
-    db.Column('review_id', db.Integer, db.ForeignKey(add_prefix_for_prod('reviews.id')), primary_key=True)
+    db.Column('review_id', db.Integer, db.ForeignKey(add_prefix_for_prod('reviews.id')), primary_key=True),
+    schema=SCHEMA if environment == "production" else None
 )
-
