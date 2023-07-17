b9abf84f3bd121ee988fd84c26b5f6fe4f6377f2
JoJo <joehearthstone@gmail.com>
2023-07-17 13:57:22 -0400

    typo

--- app/seeds/statuses.py
+++ app/seeds/statuses.py
@@ -19,7 +19,7 @@ def seed_statuses():
 
 def undo_statuses():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.stauses RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.statuses RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM statuses"))
 
