be6ba3f15e47eecbff46b4e8c32ad178564ae014
JoJo <joehearthstone@gmail.com>
2023-08-08 19:26:11 -0400

    some change idk if will use

--- react-app/src/components/EntryPage/EntryPage.js
+++ react-app/src/components/EntryPage/EntryPage.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { thunkAllEntries } from '../../store/entryReducer';
 import './EntryPage.css';
 
 
 const EntryPage = () => {
     const dispatch = useDispatch();
+    const allEntries = useSelector((state) => state.entries.allEntries.entries);
 
+    useEffect(() => {
+        dispatch(thunkAllEntries());
+    }, [dispatch]);
     return (
         <div className="entry-container">
-            <h2>Dummy Text </h2>
-            <div className="entry">
-
-            </div>
+        <h2>All Entries</h2>
+        <div className="entry-list">
+            {allEntries.map((entry) => (
+                <div key={entry.id} className="entry">
+                    <h3>{entry.game_name || 'No Game Name'}</h3>
+                    <p>System: {entry.system || 'No System Available'}</p>
+                    <p>Region: {entry.region || 'No Region Available'}</p>
+                    {/* Add other entry information */}
+                </div>
+            ))}
         </div>
-    )
+    </div>
+);
 }
 
 export default EntryPage;
